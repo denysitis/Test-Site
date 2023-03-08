bf3d2c3e9221a810a26aad1dad139404506a231e
Denys Balazh <73472742+denysitis@users.noreply.github.com>
2023-03-08 15:49:15 +0200

    Delete test-site directory

--- test-site/DB.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	$connect = mysqli_connect('localhost', 'u568759074_admin_den', 'bJ2bRa47*J', 'u568759074_denys_dataBase');
-    if (mysqli_connect_errno()) {
-        echo 'Помилка підключенння ('.mysqli_connect_errno().'): '.mysqli_connect_error();
-        exit();
-    }
-?>
