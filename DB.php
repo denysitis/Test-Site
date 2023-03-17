3372926d2e6c00779919c75030fa7019c9401fe1
Denys Balazh <73472742+denysitis@users.noreply.github.com>
2023-03-17 18:31:31 +0200

    Update DB.php

--- DB.php
+++ DB.php
@@ -1,5 +1,5 @@
 <?php 
-	$connect = mysqli_connect('localhost', 'u568759074_admin_den', 'bJ2bRa47*J', 'u568759074_denys_dataBase');
+	$connect = mysqli_connect('localhost', 'login', 'password', 'name DataBase');
     if (mysqli_connect_errno()) {
         echo 'Помилка підключенння ('.mysqli_connect_errno().'): '.mysqli_connect_error();
         exit();
